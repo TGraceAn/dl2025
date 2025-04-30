f4d91767a48e0341f92db4c7e5d4aae6872e7d45
TGraceAn <annt.bi12-008@st.usth.edu.vn>
2025-04-30 20:56:32 +0700

    Added Labwork1's code

---
+++ Labwork1.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+
+def gradient_descent(f, x_0, h, L, max_iterations, threshold = 1e-6):
+    fx = []
+    x_s = []
+    for i in range(max_iterations):
+        derivative = (f(x_0 + h) - f(x_0)) / h
+        x_a = x_0 - L * derivative
+        fx.append(f(x_a))
+        x_s.append(x_a)
+        print(f"Step {i + 1}: x = {x_a:.6f} | f(x) = {f(x_a):.2f}")
+
+        if abs(x_a - x_0) < threshold:
+            break
+        x_0 = x_a
+
+    return x_0, fx, x_s
+
+def f_x_squared(x: float) -> float:
+    return x ** 2
+
+if __name__ == "__main__":
+    result, fx, x_s = gradient_descent(
+        f=f_x_squared,
+        x_0=10.0,
+        h=0.001,
+        L=0.1,
+        max_iterations=10
+    )
+
+    # plt.plot(x_s)
+    # plt.title("Gradient Descent (x Value)")
+    # plt.xlabel("Iteration (Step)")
+    # plt.ylabel("x Value")
+    # plt.grid(True)
+    # plt.show()
+
+    # plt.plot(fx)
+    # plt.title("Gradient Descent (Gradient)")
+    # plt.xlabel("Iteration (Step)")
+    # plt.ylabel("f(x)")
+    # plt.grid(True)
+    # plt.show()
+
+    plt.figure(figsize=(10, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x_s)
+    plt.title("Gradient Descent (x Value)")
+    plt.xlabel("Iteration (Step)")
+    plt.ylabel("x Value")
+    plt.grid(True)
+    plt.subplot(1, 2, 2)
+    plt.plot(fx)
+    plt.title("Gradient Descent (Gradient)")
+    plt.xlabel("Iteration (Step)")
+    plt.ylabel("f(x)")
+    plt.grid(True)
+    plt.show()
+

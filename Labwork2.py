1895053470e06b827eec685cd7f9c359077ff5b5
TGraceAn <annt.bi12-008@st.usth.edu.vn>
2025-04-30 23:51:59 +0700

    fixed bug on central derivative

--- Labwork2.py
+++ Labwork2.py
@@ -74,10 +74,10 @@ def linear_regression_2(x, y, F, lr, iter, threshold):
         W_1_h = [W[0], W[1] + 1e-5]
 
         W_0_h_ = [W[0] - 1e-5, W[1]]
-        W_0_h_ = [W[0], W[1] - 1e-5]
+        W_1_h_ = [W[0], W[1] - 1e-5]
 
         J_w0 = (F(x, y, W_0_h) - F(x, y, W_0_h_)) / 2e-5
-        J_w1 = (F(x, y, W_1_h) - F(x, y, W_0_h_)) / 2e-5
+        J_w1 = (F(x, y, W_1_h) - F(x, y, W_1_h_)) / 2e-5
 
         # update the weights
         W[0] = W[0] - lr * J_w0
@@ -104,43 +104,41 @@ def main():
         print(x)
         print(y)
     
-    lr = 1e-4
+    lr = 3e-4
     iter = 20
 
     W_linear_1, loss_list_1, states_1 = linear_regression(x, y, F, lr, iter, 1e-5)
-    W_linear_2, loss_list_2, states_2 = linear_regression_2(x, y, F, lr, iter, 1e-5)
+    # W_linear_2, loss_list_2, states_2 = linear_regression_2(x, y, F, lr, iter, 1e-5)
     # print(W_linear_1, loss_list_1[-1], states_1[-1])
     # print("")
     # print(W_linear_2, loss_list_2[-1], states_2[-1])
 
     plt.figure(figsize=(10, 5))
-    plt.subplot(2, 2, 1)
+    plt.subplot(3, 1, 1)
     plt.plot(loss_list_1)
     plt.title("Linear Regression Loss (Analytic Gradient)")
     plt.xlabel(f"Iteration (Step), lr = {lr}")
     plt.ylabel("Loss")
     plt.grid(True)
-    plt.subplot(2, 2, 2)
-    plt.plot(loss_list_2)
-    plt.title("Linear Regression Loss (Numeric (Calulated) Gradient)")
-    plt.xlabel(f"Iteration (Step), lr = {lr}")
-    plt.ylabel("Loss")
-    plt.grid(True)
-    plt.subplot(2, 2, 3)
+    plt.subplot(3, 1, 2)
     plt.plot(states_1)
     plt.title("Linear Regression Weights (Analytic Gradient)")
     plt.xlabel(f"Iteration (Step), lr = {lr}")
     plt.ylabel("Weights")
     plt.legend(["w0", "w1"])
     plt.grid(True)
-    plt.subplot(2, 2, 4)
-    plt.plot(states_2)
-    plt.title("Linear Regression Weights (Numeric (Calulated) Gradient)")
-    plt.xlabel(f"Iteration (Step), lr = {lr}")
-    plt.ylabel("Weights")
-    plt.legend(["w0", "w1"])
+    # match final weight line with data x, y
+    plt.subplot(3, 1, 3)
+    plt.plot(x, y, 'ro', label='Data')
+    plt.plot(x, [W_linear_1[0] + W_linear_1[1]*i for i in x], 'b-', label='Linear Regression (Analytic Gradient)')
+    plt.title("Linear Regression (Analytic Gradient)")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.legend()
     plt.grid(True)
+
     plt.tight_layout()
+    plt.savefig(f"linear_regression_(lr:{lr}).png")
     plt.show()
 
 
